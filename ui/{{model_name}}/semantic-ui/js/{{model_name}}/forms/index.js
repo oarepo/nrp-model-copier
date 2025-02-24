8132a7040efed70723584b51ec35b60d90c0dda0
Mirek Simek <miroslav.simek@gmail.com>
2025-02-24 12:13:26 +0100

    javascript updates

--- ui/{{model_name}}/semantic-ui/js/{{model_name}}/forms/index.js
+++ ui/{{model_name}}/semantic-ui/js/{{model_name}}/forms/index.js
@@ -1,4 +1,6 @@
-import { createFormAppInit, parseFormAppConfig } from "@js/oarepo_ui";
+import { createFormAppInit, parseFormAppConfig } from "@js/oarepo_ui/forms";
+import FormActionsContainer from "./FormActionsContainer";
+import FormFieldsContainer from "./FormFieldsContainer";
 
 /** NOTE: This reads configuration for a form app present on a page
  *   In HTML/Jinja, represented by an element with a specific id
@@ -6,18 +8,19 @@ import { createFormAppInit, parseFormAppConfig } from "@js/oarepo_ui";
  *
  *   <div id="form-app" />
  */
-// const { formConfig } = parseFormAppConfig()
+const { formConfig } = parseFormAppConfig();
 
 /** NOTE: To customize components in a specific form app instance,
  *   you need to obtain its `overridableIdPrefix` from the corresponding config first
  */
-// const { overridableIdPrefix } = formConfig
+const { overridableIdPrefix } = formConfig;
 
 export const componentOverrides = {
   /** NOTE: Then you can then replace any existing ui
    * component with your own implementation, e.g.:
    */
-  // [`${overridableIdPrefix}.FormApp.layout`]: YourComponent,
+  [`${overridableIdPrefix}.FormActions.container`]: FormActionsContainer,
+  [`${overridableIdPrefix}.FormFields.container`]: FormFieldsContainer,
 };
 
-createFormAppInit({componentOverrides});
+createFormAppInit({ componentOverrides });
--- ui/{{model_name}}/semantic-ui/js/{{model_name}}/search/index.js
+++ ui/{{model_name}}/semantic-ui/js/{{model_name}}/search/index.js
@@ -1,26 +1,22 @@
 import {
-  // parseSearchAppConfigs,
+  parseSearchAppConfigs,
   createSearchAppsInit,
-} from "@js/oarepo_ui";
+} from "@js/oarepo_ui/search";
+import ResultsListItem from "./ResultsListItem";
 
 /** NOTE: This reads configs for any search app present on a page
  *   In HTML/Jinja, a single search app instance is typically represented
- *   as a div with a config data attribute:
- *
- *   <div data-invenio-search-config='...' />
- */
-// const [searchAppConfig, ...otherSearchAppConfigs] = parseSearchAppConfigs()
-
+@@ -14,13 +15,13 @@ import {
 /** NOTE: To customize components in a specific search app instance,
  *   you need to obtain its `overridableIdPrefix` from the corresponding config first
  */
-// const { overridableIdPrefix } = searchAppConfig
+const [{ overridableIdPrefix }] = parseSearchAppConfigs();
 
 export const componentOverrides = {
   /** NOTE: Then you can then replace any existing search ui
    * component with your own implementation, e.g.:
    */
-  // [`${overridableIdPrefix}.AutocompleteSearchBar.element`]: YourComponent,       // https://github.com/inveniosoftware/react-searchkit/blob/master/src/lib/components/AutocompleteSearchBar/AutocompleteSearchBar.js#L117-L140
+  [`${overridableIdPrefix}.ResultsList.item`]: ResultsListItem,
 };
 
 createSearchAppsInit({ componentOverrides });
