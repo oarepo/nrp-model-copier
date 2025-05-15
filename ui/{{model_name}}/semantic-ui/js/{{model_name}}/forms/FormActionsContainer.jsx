5ac7b23cd486a595f449147707e4a840341bfcab
ducica <dusanstojanovic256@gmail.com>
2025-05-15 22:20:52 +0200

    fixed form template

--- ui/{{model_name}}/semantic-ui/js/{{model_name}}/forms/FormActionsContainer.jsx
+++ ui/{{model_name}}/semantic-ui/js/{{model_name}}/forms/FormActionsContainer.jsx
@@ -1,116 +1,116 @@
 import React from "react";
 import { Card, Grid } from "semantic-ui-react";
 import {
-    PreviewButton,
-    SaveButton,
-    DeleteButton,
-    useDepositApiClient,
-    serializeErrors,
-    AccessRightField,
-    useFormConfig,
-} from "@js/oarepo_ui/forms";
+  PreviewButton,
+  SaveButton,
+  DeleteButton,
+  useDepositApiClient,
+  AccessRightField,
+  useFormConfig,
+  useSanitizeInput,
+} from "@js/oarepo_ui";
+import { ClipboardCopyButton } from "@js/oarepo_ui/components/ClipboardCopyButton";
 import { i18next } from "@translations/i18next";
 import { SelectedCommunity } from "@js/communities_components/CommunitySelector/SelectedCommunity";
 import { RecordRequests } from "@js/oarepo_requests/components";
-import { useFormikContext, setIn } from "formik";
-import { REQUEST_TYPE } from "@js/oarepo_requests_common";
+import { useFormikContext } from "formik";
+import {
+  REQUEST_TYPE,
+  beforeActionFormErrorPlugin,
+} from "@js/oarepo_requests_common";
 
 const FormActionsContainer = () => {
-    const { values, setErrors } = useFormikContext();
-    const { save } = useDepositApiClient();
-    const {
-        formConfig: {
-            permissions,
-            allowRecordRestriction,
-            recordRestrictionGracePeriod,
-        },
-    } = useFormConfig();
+  const { values, setErrors } = useFormikContext();
+  const { save } = useDepositApiClient();
+  const {
+    formConfig: {
+      permissions,
+      allowRecordRestriction,
+      recordRestrictionGracePeriod,
+    },
+  } = useFormConfig();
 
-    const onBeforeAction = ({ requestActionName, requestOrRequestType }) => {
-        const requestType =
-            requestOrRequestType?.type || requestOrRequestType?.type_id;
-        // Do not try to save in case user is declining or cancelling the request from the form
-        if (
-            requestActionName === REQUEST_TYPE.DECLINE ||
-            requestActionName === REQUEST_TYPE.CANCEL ||
-            requestType === "initiate_community_migration" ||
-            requestType === "confirm_community_migration"
-        ) {
-            return true;
-        } else {
-            return save({
-                errorMessage: i18next.t(
-                    "The request ({{requestType}}) could not be made due to validation errors. Please fix them and try again:",
-                    { requestType: requestOrRequestType.name }
-                ),
-            });
-        }
-    };
-    return (
-        <React.Fragment>
-            <Card fluid>
-                {/* <Card.Content>
-          <DepositStatusBox />
-        </Card.Content> */}
-                <Card.Content>
-                    <Grid>
-                        <Grid.Column width={16}>
-                            <div className="flex">
-                                <SaveButton fluid className="mb-10" />
-                                <PreviewButton fluid className="mb-10" />
-                            </div>
-                            {values.id && (
-                                <RecordRequests
-                                    record={values}
-                                    onBeforeAction={onBeforeAction}
-                                    onActionError={({ e, modalControl, formik }) => {
-                                        if (
-                                            e?.response?.data?.error_type === "cf_validation_error" &&
-                                            e?.response?.data?.errors
-                                        ) {
-                                            let errorsObj = {};
-                                            for (const error of e.response.data.errors) {
-                                                errorsObj = setIn(
-                                                    errorsObj,
-                                                    error.field,
-                                                    error.messages.join(" ")
-                                                );
-                                            }
-                                            formik?.setErrors(errorsObj);
-                                        } else if (e?.response?.data?.errors?.length > 0) {
-                                            const errors = serializeErrors(
-                                                e?.response?.data?.errors,
-                                                i18next.t(
-                                                    "Action failed due to validation errors. Please correct the errors and try again:"
-                                                )
-                                            );
-                                            setErrors(errors);
-                                            modalControl?.closeModal();
-                                        }
-                                    }}
-                                />
-                            )}
-                            {values.id && <DeleteButton redirectUrl="/me/records" />}
-                        </Grid.Column>
-                        <Grid.Column width={16} className="pt-10">
-                            <SelectedCommunity />
-                        </Grid.Column>
-                    </Grid>
-                </Card.Content>
-            </Card>
-            <AccessRightField
-                label={i18next.t("Access rights")}
+  const { sanitizeInput } = useSanitizeInput();
+
+  let repositoryAssignedDoi = values?.pids?.doi?.identifier;
+  // UI serialization is not passed to the form, so I think this is OK, as it is the only
+  // thing we need here currently - maybe in the future we could send the UI seiralization
+  // of record to form config
+  if (repositoryAssignedDoi && !repositoryAssignedDoi.startsWith("https")) {
+    repositoryAssignedDoi = `https://doi.org/${repositoryAssignedDoi}`;
+  }
+
+  const onBeforeAction = ({ requestActionName, requestOrRequestType }) => {
+    const requestType =
+      requestOrRequestType?.type || requestOrRequestType?.type_id;
+    // Do not try to save in case user is declining or cancelling the request from the form
+    if (
+      requestActionName === REQUEST_TYPE.DECLINE ||
+      requestActionName === REQUEST_TYPE.CANCEL ||
+      requestType === "initiate_community_migration" ||
+      requestType === "confirm_community_migration"
+    ) {
+      return true;
+    } else {
+      return save({
+        errorMessage: i18next.t(
+          "The request ({{requestType}}) could not be made due to form validation errors. Please fix them and try again:",
+          {
+            requestType:
+              requestOrRequestType?.stateful_name || requestOrRequestType.name,
+          }
+        ),
+      });
+    }
+  };
+  return (
+    <React.Fragment>
+      <Card fluid>
+        <Card.Content>
+          <Grid>
+            <Grid.Column width={16}>
+              <div className="flex">
+                <SaveButton fluid className="mb-10" />
+                <PreviewButton fluid className="mb-10" />
+              </div>
+              <RecordRequests
                 record={values}
-                labelIcon="shield"
-                fieldPath="access"
-                showMetadataAccess={permissions?.can_manage_record_access}
-                // permissions seem to not work properly when on /_new, in this case I think it is OK that you can restrict
-                // since you have access to the form
-                recordRestrictionGracePeriod={recordRestrictionGracePeriod}
-                allowRecordRestriction={allowRecordRestriction}
-            />
-        </React.Fragment>
-    );
+                onBeforeAction={onBeforeAction}
+                onErrorPlugins={[beforeActionFormErrorPlugin]}
+                actionExtraContext={{ setErrors }}
+              />
+              <DeleteButton redirectUrl="/me/records" />
+            </Grid.Column>
+            <Grid.Column width={16} className="pt-10">
+              <SelectedCommunity />
+            </Grid.Column>
+            {repositoryAssignedDoi && (
+              <Grid.Column width={16} className="pt-10">
+                <p>{i18next.t("Assigned DOI:")}</p>
+                <a
+                  href={sanitizeInput(repositoryAssignedDoi)}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  {repositoryAssignedDoi}
+                </a>{" "}
+                <ClipboardCopyButton copyText={repositoryAssignedDoi} />
+              </Grid.Column>
+            )}
+          </Grid>
+        </Card.Content>
+      </Card>
+      <AccessRightField
+        label={i18next.t("metadata/accessRights.label")}
+        record={values}
+        labelIcon="shield"
+        fieldPath="access"
+        showMetadataAccess={permissions?.can_manage_record_access}
+        recordRestrictionGracePeriod={recordRestrictionGracePeriod}
+        allowRecordRestriction={allowRecordRestriction}
+      />
+    </React.Fragment>
+  );
 };
 
 export default FormActionsContainer;
