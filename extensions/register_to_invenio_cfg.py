3f50dda2cae18c3a67fb21f6253a4e7f9802a53c
Miroslav Bauer <mirekys@gmail.com>
2025-09-08 17:14:54 +0200

    feat: register to invenio.cfg task

---
+++ extensions/register_to_invenio_cfg.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def register_model_to_invenio_cfg(model_name):
+
+    invenio_cfg_path = "invenio.cfg"
+    model_registration = f"""
+# {model_name} model registration
+from {model_name} import {model_name}_model
+
+{model_name}_model.register()
+"""
+
+    with open(invenio_cfg_path, 'a') as invenio_cfg:
+        invenio_cfg.write(model_registration)
+
+
+if __name__ == "__main__":
+    register_model_to_invenio_cfg(sys.argv[1])
