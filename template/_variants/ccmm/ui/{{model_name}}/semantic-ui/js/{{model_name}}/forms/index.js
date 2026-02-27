564a641430eb372c60ab29e7eee3126ce3d4650d
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-02-27 14:16:16 +0100

    ccmm test

--- template/_variants/ccmm/ui/{{model_name}}/semantic-ui/js/{{model_name}}/forms/index.js
+++ template/_variants/ccmm/ui/{{model_name}}/semantic-ui/js/{{model_name}}/forms/index.js
@@ -1,19 +1,29 @@
 import { DepositFormApp, parseFormAppConfig } from "@js/oarepo_ui/forms";
 import React from "react";
 import ReactDOM from "react-dom";
-import { OARepoDepositSerializer } from "@js/oarepo_ui/api";
+import { EDTFSingleDatePicker } from "@js/oarepo_ui/forms";
+import { parametrize } from "react-overridable";
+import {
+  CCMMDepositRecordSerializer,
+  CCMMSections,
+} from "@js/ccmm_invenio/forms";
 import FormFieldsContainer from "./FormFieldsContainer";
 
-const recordSerializer = new OARepoDepositSerializer(
-  ["errors", "expanded"],
-  ["__key"]
+const { rootEl, config, ...rest } = parseFormAppConfig();
+const recordSerializer = new CCMMDepositRecordSerializer(
+  config.default_locale,
+  config.custom_fields.vocabularies,
 );
 
-const { rootEl, config, ...rest } = parseFormAppConfig();
+const parametrizeEDTFSingleDatePicker = parametrize(EDTFSingleDatePicker, {
+  customInputProps: { width: 16 },
+});
 
 const overridableIdPrefix = config.overridableIdPrefix;
 
 export const componentOverrides = {
+  "InvenioRdmRecords.DepositForm.DatesField.DateField":
+    parametrizeEDTFSingleDatePicker,
   [`${overridableIdPrefix}.FormFields.container`]: FormFieldsContainer,
 };
 
@@ -21,8 +31,10 @@ ReactDOM.render(
   <DepositFormApp
     config={config}
     {...rest}
+    sections={CCMMSections}
     recordSerializer={recordSerializer}
     componentOverrides={componentOverrides}
+    useWizardForm
   />,
-  rootEl
+  rootEl,
 );
