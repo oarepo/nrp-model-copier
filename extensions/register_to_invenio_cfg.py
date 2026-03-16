1bd6b7c3da9fa32c703310b63797d0eef4d08b85
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-03-16 10:53:50 +0100

    nesting model BE part into models folder

--- extensions/register_to_invenio_cfg.py
+++ extensions/register_to_invenio_cfg.py
@@ -6,13 +6,13 @@ def register_model_to_invenio_cfg(model_name, endpoint):
     invenio_cfg_path = "invenio.cfg"
     model_registration = f"""
 # {model_name} model registration
-from {model_name} import {model_name}_model
-
+from models.{model_name} import {model_name}_model
+# TODO: remove this once everyone is migrated to oarepo-ui >=7
 {model_name}_model.register()
 DASHBOARD_RECORD_CREATE_URL = "/{endpoint}/uploads/new"
 """
 
-    with open(invenio_cfg_path, 'a') as invenio_cfg:
+    with open(invenio_cfg_path, "a") as invenio_cfg:
         invenio_cfg.write(model_registration)
 
 
