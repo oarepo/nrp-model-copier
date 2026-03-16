221a268c56969b50f58397e6e8e098a6165dbfee
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-03-16 13:12:01 +0100

    add models module

--- extensions/register_to_invenio_cfg.py
+++ extensions/register_to_invenio_cfg.py
@@ -1,15 +1,13 @@
 import sys
 
 
-def register_model_to_invenio_cfg(model_name, endpoint):
+def register_model_to_invenio_cfg(model_name):
 
     invenio_cfg_path = "invenio.cfg"
     model_registration = f"""
 # {model_name} model registration
 from models.{model_name} import {model_name}_model
-# TODO: remove this once everyone is migrated to oarepo-ui >=7
 {model_name}_model.register()
-DASHBOARD_RECORD_CREATE_URL = "/{endpoint}/uploads/new"
 """
 
     with open(invenio_cfg_path, "a") as invenio_cfg:
@@ -17,4 +15,4 @@ DASHBOARD_RECORD_CREATE_URL = "/{endpoint}/uploads/new"
 
 
 if __name__ == "__main__":
-    register_model_to_invenio_cfg(sys.argv[1], sys.argv[2])
+    register_model_to_invenio_cfg(sys.argv[1])
