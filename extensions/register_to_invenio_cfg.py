e132e4b4855d5222e9bbe2c745c30e4449a2fced
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-06-29 08:59:10 +0200

    fixed multiple model registration

--- extensions/register_to_invenio_cfg.py
+++ extensions/register_to_invenio_cfg.py
@@ -4,12 +4,20 @@ import sys
 def register_model_to_invenio_cfg(model_name):
 
     invenio_cfg_path = "invenio.cfg"
+    marker = f"# {model_name} model registration"
     model_registration = f"""
-# {model_name} model registration
+{marker}
 from models.{model_name} import {model_name}_model
 
 {model_name}_model.register()
 """
+
+    try:
+        with open(invenio_cfg_path, "r") as invenio_cfg:
+            if marker in invenio_cfg.read():
+                return
+    except FileNotFoundError:
+        pass
 
     with open(invenio_cfg_path, "a") as invenio_cfg:
         invenio_cfg.write(model_registration)
